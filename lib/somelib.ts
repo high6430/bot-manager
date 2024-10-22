25f70f2a86b4946a773cd6ebadcab7be1a82997d
suke <akitochuanshang@gmail.com>
2024-10-22 10:47:57 +0900

    通信型定義

--- lib/somelib.ts
+++ lib/somelib.ts
@@ -1,5 +1,11 @@
 //両方で使う細かいライブラリ
 //クライアント側のコードを含む
+export interface basejson {
+    //クライアントとサーバーの通信内容定義
+    code: number,
+    UUID: `${string}-${string}-${string}-${string}-${string}` | null
+    msg: string
+}
 class somelib {
 
     constructor() { };
